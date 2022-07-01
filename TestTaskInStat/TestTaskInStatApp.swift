4ddb40ad00bd7a879f68127ae40926dddb9064f9
denis <9172885049@mail.ru>
2022-07-01 10:58:29 +0300

    initial commit

---
+++ TestTaskInStat/TestTaskInStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskInStatApp.swift
+//  TestTaskInStat
+//
+//  Created by admin on 01.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskInStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
